b617b7d38426d8644b67fafa3b0df6525fc307d0
MsegalLeeds <nxcj0989@leeds.ac.uk>
2026-03-03 13:42:27 +0000

    Completed worksheet

--- worksheet/task1/task1.1.c
+++ worksheet/task1/task1.1.c
@@ -10,6 +10,21 @@
 #include <stdio.h>
 
 int main(void) {
-    
-	return 0;
+
+    for (int n = 2; n <= 100; n++) {
+        int isPrime = 1;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            printf("%d\n", n);
+        }
+    }
+
+    return 0;
 }
--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,24 +2,38 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID: 
+ * Name: Marc Segal
+ * ID: 201934205
  */
 
 #include <stdio.h>
 #include <string.h>
 
 int main(void){
-	long decimal=0;
-	char hex[9];
-	
-	printf("Enter a hexadecimal:");
+    long decimal = 0;
+    char hex[9];
 
-	// if input contains invalid hex digit
-	// printf("Error: Invalid Hexadecimal\n");
-	
-	// print the decimal result
-	printf("decimal:%ld\n", decimal);
-	
-	return 0;
+    printf("Enter a hexadecimal:");
+    scanf("%8s", hex);
+
+    for (int i = 0; i < strlen(hex); i++) {
+        int value;
+
+        if (hex[i] >= '0' && hex[i] <= '9')
+            value = hex[i] - '0';
+        else if (hex[i] >= 'A' && hex[i] <= 'F')
+            value = hex[i] - 'A' + 10;
+        else if (hex[i] >= 'a' && hex[i] <= 'f')
+            value = hex[i] - 'a' + 10;
+        else {
+            printf("Error: Invalid Hexadecimal\n");
+            return 0;
+        }
+
+        decimal = decimal * 16 + value;
+    }
+
+    printf("decimal:%ld\n", decimal);
+
+    return 0;
 }
